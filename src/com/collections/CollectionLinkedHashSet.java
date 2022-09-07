69af56f4623685b403dbd3f8a167129e28392166
BY-Manish-Kushwaha <manish.kushwaha@blueyonder.com>
2022-09-07 11:00:19 +0530

    MAC_Update-005

--- src/com/collections/CollectionLinkedHashSet.java
+++ src/com/collections/CollectionLinkedHashSet.java
@@ -1,33 +1,47 @@
 package com.collections;
 import java.util.*;
 public class CollectionLinkedHashSet {
-    public void display(LinkedHashSet<String> str, String name){
+    public static void display(LinkedHashSet<String> str, String name){
         System.out.println("-----------"+name+"-----------");
         Iterator <String> itr = str.iterator();
         int index=0;
         while(itr.hasNext()){
-            System.out.println(index+". -> "+itr.next());
+            String var = itr.next();
+            System.out.println(index+". -> value = "+var+"  , HashCode = "+var.hashCode());
             index++;
         }
         System.out.println("------------ END ----------");
 
     }
     public static void main(String[] args) {
-        CollectionLinkedHashSet obj = new CollectionLinkedHashSet();
         // Always maintains the sequence of Set.
         // Order of elements
         // Double linked list across all elements
         LinkedHashSet<String> dlset = new LinkedHashSet<>();
 
         // adding
+        dlset.add("-3");
+        dlset.add("-2");
+        dlset.add("-1");
+        dlset.add("0");
+        //dlset.add(null); //error
         dlset.add("1");
         dlset.add("2");
         dlset.add("3");
         dlset.add("4");
 
         // display
-        obj.display(dlset,"LinkedHashSet");
+        display(dlset,"LINKEDHASHSET");
         System.out.println("LinkedHashSet elements: "+dlset);
+
+        System.out.println("isContains "+dlset.contains("0"));
+
+        System.out.println("isEmpty() "+dlset.isEmpty() );
+
+        System.out.println("size() " + dlset.size());
+
+        dlset.clear();
+        display(dlset,"CLEARED - LINKEDHASHSET");
 
 
 
--- src/com/collections/CollectionTreeSet.java
+++ src/com/collections/CollectionTreeSet.java
@@ -1,7 +1,9 @@
 package com.collections;
+import com.sun.source.tree.Tree;
+
 import java.util.*;
-public class CollectionHeshSet {
-    public static void display(HashSet<String> str, String name){
+public class CollectionTreeSet {
+    public static void display(TreeSet<String> str, String name){
         System.out.println("-----------"+name+"-----------");
         Iterator <String> itr = str.iterator();
         int index=0;
@@ -14,10 +16,7 @@ public class CollectionHeshSet {
 
     }
     public static void main(String[] args) {
-        // Allows null values
-        // Doesn't allow duplicated
-
-        HashSet<String> hash = new HashSet<>();
+        TreeSet<String> hash = new TreeSet();
         hash.add("-3");
         hash.add("-2");
         hash.add("-1");
@@ -30,10 +29,10 @@ public class CollectionHeshSet {
         hash.add("4");
         hash.add("5");
         hash.add("6");
-
+        hash.addAll(List.of(new String[]{"99", "12"}));
         // display
-        display(hash,"HASHSET");
-        System.out.println("LinkedHashSet elements: "+hash);
+        display(hash,"TreeSET");
+        System.out.println(" Sorted TreeSet elements: "+hash);
 
         System.out.println("isContains "+hash.contains("0"));
 
@@ -42,19 +41,20 @@ public class CollectionHeshSet {
         System.out.println("size() " + hash.size());
 
         hash.remove("2");
-        display(hash,"removed 2 - HASHSET");
+        display(hash,"removed 2 - TreeSET");
 
         //clone
-        HashSet<String> clone = (HashSet<String>) hash.clone();
-        display(clone,"CLONE - HASHSET");
+        TreeSet<String> clone = (TreeSet<String>) hash.clone();
+        display(clone,"CLONE - TreeSET");
 
 
+        // first
+        System.out.println("First -> "+hash.first());
+
+        // last
+        System.out.println("Last -> "+hash.last());
 
         hash.clear();
-        display(hash,"CLEARED - HASHSET");
-        
-
-
-
+        display(hash,"CLEARED - TreeSET");
     }
 }
